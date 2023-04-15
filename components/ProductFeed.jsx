071dcd4b67ca51026a6f979978a3a71dfb5d2cd2
Tunahanclr <tunaceler@gmail.com>
2023-04-15 21:34:07 +0300

    finiish

--- components/ProductFeed.jsx
+++ components/ProductFeed.jsx
@@ -1,28 +1,32 @@
 import Products from "./Products";
+
 export default function ProductFeed({ products }) {
-    return (
-      <>
+  return (
+    <>
       <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center md:-mt-56">
-        {products.slice(0,4).map((product) => (
-       <div>
-        <Products  key={product.id} product={product}/>
-       </div>
+        {products.slice(0, 4).map((product) => (
+          <div key={product.id}>
+            <Products product={product} />
+          </div>
         ))}
-        <img className="md:col-span-full mx-auto" src="https://links.papareact.com/dyz" alt="" />
+        <img
+          className="md:col-span-full mx-auto"
+          src="https://links.papareact.com/dyz"
+          alt=""
+        />
         <div className="md:col-span-2">
-        {products.slice(4,5).map((product) => (
-       <div>
-        <Products  key={product.id} product={product}/>
-       </div>
-        ))}
+          {products.slice(4, 5).map((product) => (
+            <div key={product.id}>
+              <Products product={product} />
+            </div>
+          ))}
         </div>
-        {products.slice(5,products.length).map((product) => (
-       <div>
-        <Products  key={product.id} product={product}/>
-       </div>
+        {products.slice(5, products.length).map((product) => (
+          <div key={product.id}>
+            <Products product={product} />
+          </div>
         ))}
       </div>
-      </>
-    );
-  }
-  
+    </>
+  );
+}
