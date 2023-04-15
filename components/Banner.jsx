e03f19030e26df06f84696ca38c988505b99a170
Tunahanclr <tunaceler@gmail.com>
2023-04-15 22:26:51 +0300

    son

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-
+import Image from 'next/image';
 
 export default function Banner() {
   return (
@@ -14,11 +14,11 @@ export default function Banner() {
         showThumbs={false}
         interval={5000}
         >
-        <div><img loading='lazy' src="https://m.media-amazon.com/images/I/71tIrZqybrL._SX3000_.jpg" alt="" /></div>
-        <div><img loading='lazy' src="	https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3000_.jpg" alt="" /></div>
-        <div><img loading='lazy' src="https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg" alt="" /></div>
-        <div><img loading='lazy' src="https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg" alt="" /></div>
-        <div><img loading='lazy' src="	https://m.media-amazon.com/images/I/71qid7QFWJL._SX3000_.jpg" alt="" /></div>
+<div><img loading='lazy' src='https://m.media-amazon.com/images/I/71tIrZqybrL._SX3000_.jpg' alt='' /></div>
+<div><img loading='lazy' src='https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3000_.jpg' alt='' /></div>
+<div><img loading='lazy' src='https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg' alt='' /></div>
+<div><img loading='lazy' src='https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg' alt='' /></div>
+
 
         </Carousel>
     </div>
